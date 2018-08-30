4db66267d17f39a3ad1aff4733b721cebf7ed5fa
Tushar Nema <tusharnema@gmail.com>
2018-08-30 18:57:01 +0530

    Updated binary search tree with count

--- mypythontree.py
+++ mypythontree.py
@@ -3,47 +3,24 @@ class Node:
         self.data=data
         self.left=None
         self.right=None
+        self.count=1
  
 def inorder(root):
     if root:
         inorder(root.left)
-        print(root.data)
+        print(root.data,"-->",root.count)
         inorder(root.right)
  
-##def insert(root,data):
-##    #print(root,data)
-##    if root is None:
-##        root=Node(data)
-##    elif root.data>data:
-##        root.left=insert(root.left,data)
-##    else:
-##        root.right=insert(root.right,data)
-##
-##    return root
- 
- 
- 
-def insert(root,key):
-    curr = root;
-    parent = None;
-    if (root  is None):
-        return Node(key)
- 
- 
-    while curr is not None:
-        parent = curr;
-        if (key < curr.data):
-            curr = curr.left
-        else:
-            curr = curr.right;
-    if (key < parent.data):
-        parent.left =Node(key);
-    else:
-        parent.right = Node(key);
- 
-    return root;
- 
+def insert(root,data):
+    #print(root,data)
+    if root is None:
+        root=Node(data)
     
+    elif root.data>data:
+        root.left=insert(root.left,data)
+    else:
+        root.right=insert(root.right,data)
+    return root
 def search(root,key):
     if root==None or root.data==key:
         return root
@@ -54,19 +31,20 @@ def search(root,key):
             return search(root.right,key)
  
  
-def getMax(root):
-    prev=None
-    while root:
-        prev=root
-        root=root.right
-    return prev.data if prev is not None else -1
  
 def getMin(root):
     prev=None
     while root:
         prev=root
         root=root.left
-    return prev.data if prev is not None else -1
+    return prev
+ 
+def getMax(root):
+    prev=None
+    while root:
+        prev=root
+        root=root.right
+    return prev
  
 def height(root):
     
@@ -87,9 +65,9 @@ def delete(root,data):
             return root.right
         if root.right is None:
             return root.left
-        m=getMin(root.right)
-        root.data=m
-        root.right=delete(root.right,m)
+        m=min(root.right)
+        root.data=m.data
+        root.right=delete(root.right,m.data)
     return root
  
  
@@ -106,4 +84,49 @@ def lcm(root,key1,key2):
             return lcm(root.left,key1,key2)
         else:
             return lcm(root.right,key1,key2)
+ 
+ 
+    
+q=int(input())
+root=None
+for i in range(q):
+    input_str=input()
+    if input_str=='CountHigh':
+        print(getMax(root).count if root is not None else -1)
+ 
+    elif input_str=='CountLow':
+        print(getMin(root).count if root is not None else -1)
+ 
+    elif input_str=='Diff':
+        ma=getMax(root)
+        mi=getMin(root)
+        if ma is None and mi is None:
+            print(-1)
+        elif ma.data!=mi.data:
+            print(ma.data-mi.data)
+            if ma.count==1:
+                root=delete(root,ma.data)
+            else:
+                ma.count-=1
+ 
+            if mi.count==1:
+                root=delete(root,mi.data)
+            else:
+                mi.count-=1
+                
+        
+        else:
+            print(0)
+            if mi.count==1:
+                root=delete(root,ma.data)
+            else:
+                mi.count-=1
+ 
+    else:
+        data=int(input_str.split()[1])
+        tmp=search(root,data)
+        if tmp is None:
+            root=insert(root,data)
+        else:
+            tmp.count+=1
  
