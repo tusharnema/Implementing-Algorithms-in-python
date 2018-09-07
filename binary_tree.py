b28da701bc62eb45dfcf2465bf355e8d92f39cd6
Tushar Nema <tusharnema@gmail.com>
2018-09-07 11:40:52 +0530

    Add files via upload

---
+++ binary_tree.py
@@ -0,0 +1,30 @@
+from collections import deque
+class Node:
+	def __init__(self,data):
+		self.data=data
+		self.left=None
+		self.right=None
+
+
+def printLevelOrder(root):
+	dq=deque()
+	temp=root
+
+	while temp is not None:
+		print(temp.data)
+		dq.append(temp.left)
+		dq.append(temp.right)
+		temp=dq.popleft()
+
+
+
+root=Node(1)
+
+root.left=Node(2)
+root.right=Node(3)
+root.left.left=Node(7)
+root.left.right=Node(4)
+root.right.left=Node(6)
+root.right.right=Node(10)
+
+printLevelOrder(root)
