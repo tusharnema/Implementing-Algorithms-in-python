3f38a068f4320a3e137ff094106da34b2b0ce4b5
Tushar Nema <tusharnema@gmail.com>
2018-09-16 10:47:39 +0530

    Update binary_tree.py

--- binary_tree.py
+++ binary_tree.py
@@ -39,12 +39,4 @@ def getleafcount(root):
 root=Node(1)
 
 root.left=Node(2)
-root.right=Node(3)
-root.left.left=Node(7)
-root.left.right=Node(4)
-root.right.left=Node(6)
-root.right.right=Node(10)
 
-#printLevelOrder(root)
-print(height(root))
-print(getleafcount(root))
